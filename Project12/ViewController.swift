bcfd4fbd2c39374f0e634c99bfd9cb4aea80fe98
Valeriy Kovalevskiy <v.kovalevskiy.ios@gmail.com>
2020-04-14 10:04:15 +0300

    Project12

--- Project12/ViewController.swift
+++ Project12/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let defaults = UserDefaults.standard
+        
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(2.0, forKey: "Age")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "array")
+        
+        let dict = ["name" : "paul" , "country" : "UK"]
+        defaults.set(dict, forKey: "dictionary")
+        
+        let savedArray = defaults.object(forKey: "array") as? [String] ?? [String]()
+        let savedDictionary = defaults.object(forKey: "dictionart") as? [String : String] ?? [String : String]()
+        
     }
 
 
